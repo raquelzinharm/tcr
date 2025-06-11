ab7b95fa3fe0fdef12b68a164f49e68048cbfc41
raquelzinharm <raquelrmelo01@gmail.com>
2025-06-10 21:41:18 -0300

    busca

--- resources/views/welcome_resultado_busca.blade.php
+++ resources/views/welcome_resultado_busca.blade.php
@@ -29,32 +29,6 @@
             </form>
         </nav>
 
-
-        @if($postagens->isEmpty())
-            <p>Nenhuma receita encontrada.</p>
-        @else
-            @foreach ($postagens as $postagem)
-            <div class="card mb-3">
-                <div class="card-header">
-                    <strong>{{ $postagem->titulo }}</strong>
-                </div>
-                <div class="card-body">
-                    <p><strong>Categoria:</strong> {{ $postagem->categoria->nome ?? 'Sem categoria' }}</p>
-                    <p>{!! \Illuminate\Support\Str::limit(strip_tags($postagem->descricao), 150) !!}</p>
-                    <a href="{{ url('postagem/' . $postagem->id) }}" class="btn btn-primary">Ver receita</a>
-                </div>
-                <div class="card-footer text-muted">
-                    Publicado por: {{ $postagem->autor->name ?? 'Usuário não encontrado' }} em {{ $postagem->created_at->format('d/m/Y') }}
-                </div>
-            </div>
-            @endforeach
-
-            <!-- Paginação -->
-            <div class="d-flex justify-content-center">
-                {{ $postagens->links() }}
-            </div>
-        @endif
-
     </div>
 @endsection
 
